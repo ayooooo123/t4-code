c557274f59097cc6c15771c04ec5fdc5f63190c1
jd1100 <248791769+ayooooo123@users.noreply.github.com>
2026-07-14 19:50:09 -0400

    feat: add peer invite codec

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,1 +1,3 @@
 export * from "@oh-my-pi/app-wire";
+export { decodePeerInvite, encodePeerInvite, peerInviteMetadata } from "./peer-invite.ts";
+export type { PeerInvite, PeerInviteMetadata } from "./peer-invite.ts";
